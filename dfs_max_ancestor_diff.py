38f2ad868a2d0b1a175868d03b90f7124fb8bba9
Jonathan Pavkov <jonathan.pavkov@gmail.com>
2024-02-08 11:05:21 -0500

    max diff update

--- dfs_max_ancestor_diff.py
+++ dfs_max_ancestor_diff.py
@@ -43,4 +43,17 @@ class Solution:
         helper(root, root.val, root.val)
         return self.result
 
+# [8,3,10,1,6,null,14,null,null,4,7,13]
+root = TreeNode(8)
+root.left = TreeNode(3)
+root.right = TreeNode(10)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(6)
+root.left.right.left = TreeNode(4)
+root.left.right.right = TreeNode(7)
+root.right.right = TreeNode(14)
+root.right.right.left = TreeNode(13)
+
+sol = Solution()
+print(sol.maxAncestorDiff(root))
 
