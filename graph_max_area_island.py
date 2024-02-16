e3db91697d133d68e0f99349376f4c8593745374
Jonathan Pavkov <jonathan.pavkov@gmail.com>
2024-02-16 11:32:35 -0500

    graphs 3

--- graph_max_area_island.py
+++ graph_max_area_island.py
@@ -28,7 +28,28 @@ grid[i][j] is either 0 or 1.
 
 class Solution:
     def maxAreaOfIsland(self, grid: list[list[int]]) -> int:
-        pass
+
+        seen = set()
+
+        def dfs(grid, i, j):
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0 or (i, j) in seen:
+                return 0
+            #grid[i][j] = 0 # turn square to 0 so already viewed
+            seen.add((i, j))
+            area = 1
+            area += dfs(grid, i+1, j)
+            area += dfs(grid, i-1, j)
+            area += dfs(grid, i, j+1)
+            area += dfs(grid, i, j-1)
+            return area
+        
+        maxArea = 0
+        for i, row in enumerate(grid):
+            for j, cell in enumerate(row):
+                if cell == 1:
+                    maxArea = max(maxArea, dfs(grid, i, j))
+        
+        return maxArea
 
 grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
         [0,0,0,0,0,0,0,1,1,1,0,0,0],
@@ -39,6 +60,9 @@ grid = [[0,0,1,0,0,0,0,1,0,0,0,0,0],
         [0,0,0,0,0,0,0,1,1,1,0,0,0],
         [0,0,0,0,0,0,0,1,1,0,0,0,0]]
 
-print(grid[0][2])
-print(len(grid))
-print(len(grid[0]))
+sol = Solution()
+print(sol.maxAreaOfIsland(grid))
+
+# print(grid[0][2])
+# print(len(grid))
+# print(len(grid[0]))
